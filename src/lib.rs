1c4b7c555587583a7bf4121b7503ad938b3d601c
Adam Szopa <adam@szopa.org.pl>
2017-02-21 18:53:45 +0100

    Add documentation for calculate_squares.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![crate_name = "sudoku"]
+
 pub fn print_sudoku(puzzle: &[u32]) {
 
     let (big_dim, check_small, small_dim) = calculate_squares(&puzzle).unwrap();
@@ -23,7 +25,19 @@ fn test_print(){
     print_sudoku(&vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
 }
 
-fn calculate_squares(puzzle: &[u32]) -> Option<(u32, bool, u32)> {
+/// Returns a Some with the lenght of a given sudoku puzzle, a bool
+/// which indicates if there are inner squares and the size of the small one.
+/// Note: The value of the smaller one might be nonsense if the bool is false.
+///
+/// # Examples
+///
+/// ```
+/// use sudoku::calculate_squares;
+/// 
+/// let sudoku = vec![1,2,3,4];
+/// let (big_dim, check_small, small_dim) = calculate_squares(&sudoku).unwrap();
+/// ```
+pub fn calculate_squares(puzzle: &[u32]) -> Option<(u32, bool, u32)> {
     let big_dim = f64::sqrt(puzzle.len() as f64) as u32;
     if big_dim * big_dim != puzzle.len() as u32 {
         return None;
