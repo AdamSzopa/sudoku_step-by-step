cf5795b24fd6cd204f99621a36e4022f5aa22572
Adam Szopa <adam@szopa.org.pl>
2017-02-21 14:51:47 +0100

    Add print.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+pub fn print_sudoku(puzzle: &[u32]) {
+ //how do I know how big is the square?
+ //does it have inner squares?   
 }
