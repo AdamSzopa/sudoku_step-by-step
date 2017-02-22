15c4eef63c002370069e7eea52130dbc62d100ca
Adam Szopa <adam@szopa.org.pl>
2017-02-22 08:49:26 +0100

    Benchmark for solve.

--- src/bench.rs
+++ src/bench.rs
@@ -19,5 +19,27 @@ fn unique_large(bench: &mut Bencher) {
     bench.iter(|| check_if_unique(&vec))
 }
 
+fn solve_small_simple(bench: &mut Bencher) {
+    let vec = vec![0;25];
+    bench.iter(|| solve(&vec))
+}
+
+fn solve_normal_simple(bench: &mut Bencher) {
+    let vec = vec![0;81];
+    bench.iter(|| solve(&vec))
+}
+
+fn solve_normal(bench: &mut Bencher) {
+    let vec = vec![0, 0, 7, 0, 2, 0, 0, 0, 3, 8, 0, 5, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 6, 0, 1, 5, 8, 0, 0, 0, 0, 3, 0, 0, 6, 0, 0, 0, 1, 7, 0, 0, 0, 0, 9, 0,
+                   0, 0, 2, 9, 0, 0, 0, 6, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0,
+                   8, 0, 7];
+    bench.iter(|| solve(&vec))
+}
+
 benchmark_group!(unique, unique_small, unique_medium, unique_large);
-benchmark_main!(unique);
+benchmark_group!(solver,
+                 solve_small_simple,
+                 solve_normal_simple,
+                 solve_normal);
+benchmark_main!(unique, solver);
